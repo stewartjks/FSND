9edf8c0b797ca468e2a8f1d5e7688ee72d37c0ad
Jack Stewart <stewartjks@gmail.com>
2020-01-07 17:52:28 -0800

    fixed form submit issues for all three  requests

--- projects/01_fyyur/app.py
+++ projects/01_fyyur/app.py
@@ -226,7 +226,7 @@ def create_venue_submission():
   if error:
     abort(400)
   else:
-    flash('Venue ' + request.form['name'] + ' was successfully listed!')
+    flash('Venue ' + venue_name + ' was successfully listed!')
     return render_template('pages/home.html')
 
 @app.route('/venues/<venue_id>', methods=['DELETE'])
@@ -422,13 +422,9 @@ def create_show_submission():
   error = False
   body = {}
   try:
-    print('Request received')
     show_artist_id = request.get_json()['artist_id']
-    print(show_artist_id)
     show_venue_id = request.get_json()['venue_id']
-    print(venue_id)
     show_start_time = request.get_json()['start_time']
-    print(start_time)
     new_show = Show(artist_id = show_artist_id, venue_id = show_venue_id, start_time = show_start_time)
     print(new_show)
     # Add new artist record to db
